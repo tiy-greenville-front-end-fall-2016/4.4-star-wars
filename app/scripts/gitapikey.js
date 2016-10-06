652101505195341370757af8377159089995c34b
Dan Dietz <danddietz@gmail.com>
2016-10-06 12:21:09 -0400

    rest api demo

---
+++ app/scripts/gitapikey.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+  "token": '21927ccaa4fdbd1e7a39eec521b88fa38718d1ee'
+};
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var _ = require('underscore');
+var Handlebars = require('handlebars');
+var githubtoken = require('./gitapikey.js');
+
+if(githubtoken !== undefined){
+  $.ajaxSetup({
+    headers: {
+      'Authorization': 'token ' + githubtoken.token
+    }
+  });
+}
+
+
+$.ajax('http://swapi.co/api/planets/').then(displayPlanetList);
+
+function displayPlanetList(data){
+  var planets = data.results;
+  var $planetContainer = $('#planet-list');
+
+  var source = $('#plaent-template').html();
+  var template = Handlebars.compile(source);
+
+  _.each(planets, function(planet){
+    $planetContainer.append(template(planet));
+  });
+}
